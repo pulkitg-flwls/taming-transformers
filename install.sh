cb8aaeac75ce66634a53e9a6ab148a569269b72b
pulkitg-flwls <pulkit.gera@flawlessai.com>
2025-04-09 19:59:27 +0000

    basic code running

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ pip install albumentations==0.4.3
 pip install omegaconf==2.0.0
 pip uninstall transformers
 pip install transformers==4.31.0 tokenizers==0.13.3 --prefer-binary
-pip install pytorch-lightning==1.0.8
+pip install pytorch-lightning==1.5.0
 pip install test-tube==0.7.5
